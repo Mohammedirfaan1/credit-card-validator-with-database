61dc4889fa36ef014f49f844b8d8189fe261cd93
Mohammed Irfaan <90182329+Mohammedirfaan1@users.noreply.github.com>
2023-05-01 12:29:53 +0530

    Update test_credit_card_validator.py

--- test_credit_card_validator.py
+++ test_credit_card_validator.py
@@ -1,17 +1,22 @@
 import pytest
 from credit_card_validator import CreditCardValidator
 
+from credit_card_validator import CreditCardValidator
+
 def test_validate_card_number():
     # Arrange
-    card_number = "4929537721266964"
-    name = "Neymar"
-    validator = CreditCardValidator(card_number, name)
+    cards = [("4929537721266964", "Neymar"),
+             ("4716111049662895", "Mbappe"),
+             ("5574552811652176", "Ziyech"),
+             ("370777153052423", "Mosalah"),
+             ("374245455400126", "Coutihno"),
+             ("342658788906503", "Kante")
+            ]
 
-    # Act
-    result = validator.validate()
-
-    # Assert
-    assert result == True
+    # Act and Assert
+    for card_number, name in cards:
+        validator = CreditCardValidator(card_number, name)
+        assert validator.validate() == True
 
 
 
