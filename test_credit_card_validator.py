fd5e68476e1ce5a95138a2d61d2dea4b7583aa57
Mohammed Irfaan <90182329+Mohammedirfaan1@users.noreply.github.com>
2023-05-01 12:12:57 +0530

    Update test_credit_card_validator.py

--- test_credit_card_validator.py
+++ test_credit_card_validator.py
@@ -2,24 +2,16 @@ import pytest
 from credit_card_validator import CreditCardValidator
 
 def test_validate_card_number():
-    validator = CreditCardValidator("4929537721266964", name="Neymar")
-    assert validator.validate() == "Valid"
-    validator = CreditCardValidator("4716111049662895", name="Mbappe")
-    assert validator.validate() == "Valid"
-    validator = CreditCardValidator("5574552811652176", name="Ziyech")
-    assert validator.validate() == "Valid"
-    validator = CreditCardValidator("370777153052423", name="Mosalah")
-    assert validator.validate() == "Valid"
-    validator = CreditCardValidator("342658788906503", name="Kante")
-    assert validator.validate() == "Valid"
-    validator = CreditCardValidator("374245455400126", name="Coutihno")
-    assert validator.validate() == "Valid"
-    validator = CreditCardValidator("3742454", name="pete")
-    assert validator.validate() == "Invalid"
-    validator = CreditCardValidator("545540012600000000", name="Copper")
-    assert validator.validate() == "Invalid"
-    validator = CreditCardValidator("07866_+0126", name="nop")
-    assert validator.validate() == "Invalid"
+    # Arrange
+    card_number = "4929537721266964"
+    name = "Neymar"
+    validator = CreditCardValidator(card_number, name)
+
+    # Act
+    result = validator.validate()
+
+    # Assert
+    assert result == True
 
 
 
